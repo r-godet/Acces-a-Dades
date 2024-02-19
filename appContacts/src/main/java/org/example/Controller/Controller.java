e5af9c4b6208d10f84738c3828f8c552cb1bc348
r-godet <rutxi187@gmail.com>
2024-02-19 10:27:27 +0100

    j

--- appContacts/src/main/java/org/example/Controller/Controller.java
+++ appContacts/src/main/java/org/example/Controller/Controller.java
@@ -1,15 +1,16 @@
 package org.example.Controller;
 import org.example.Entities.User;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Controller {
 
    static User user;
-    public static void main(String[] args){
-        User.preguntas();
+    public static void main(String[] args)
+    {
+        
     }
     public Controller(User user) {
-        Controller.user = user;
-        User.preguntas();
+
 
     }
 }
--- appContacts/src/main/java/org/example/Entities/User.java
+++ appContacts/src/main/java/org/example/Entities/User.java
@@ -9,27 +9,13 @@ public class User {
     static String foto;
 
     public static void main(String[] args){
-        preguntas();
+
     }
-    public static void preguntas(){
-        boolean ponerFoto;
-        System.out.print("\nNombre: ");
-        nombre = lector.nextLine();
 
-        System.out.print("Apellido: ");
-        apellido = lector.nextLine();
-
-        System.out.print("Numero de telefono: ");
-        numero = lector.nextLine();
-
-        System.out.print("Quieres añadir una foto? (Si o No): ");
-        foto = lector.nextLine();
-
-        if(foto.equals("Si")) {
-            ponerFoto = true;
-            System.out.print("Se añadira una foto");
-        }else{
-            ponerFoto = false;
-        }
+    public User(String nombre, String apellido, String numero, String foto){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numero = numero;
+        this.foto = foto;
     }
 }
